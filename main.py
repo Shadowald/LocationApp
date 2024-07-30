7e04d83300146a227293f409ff1caab24840a4fc
Forest Feltner <ffeltner@strauss-pack.com>
2024-07-30 15:43:57 +0200

    Setup basic start
    Wrote basic start for app to include MDApp and Builder

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class LocationApp(MDApp):
+
+    def build(self):
+        screen = Builder.load_string('')
+
+        return screen
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+LocationApp().run()
