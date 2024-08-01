927f4e8fd67593eff46a4f40615f3da312df8899
Forest Feltner <ffeltner@strauss-pack.com>
2024-08-01 12:30:01 +0200

    Add login function
    Made function so that login button works and saves input text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 helper = """
 Screen:
@@ -16,22 +17,27 @@ Screen:
         height: self.parent.height / 3
         
     MDTextField:
-        hint_text: "Enter Username"
-        helper_text_mode: "on_focus"
+        id: user_email
+        hint_text: "Enter Email"
+        helper_text: "Please enter an email address"
+        helper_text_mode: "on_error"
         pos_hint: {'center_x': 0.5, 'center_y': 0.5}
         size_hint_x: None
         width: self.parent.width / 1.5
         
     MDTextField:
+        id: user_password
         hint_text: "Enter Password"
-        helper_text_mode: "on_focus"
-        pos_hint: {'center_x': 0.5, 'center_y': 0.4}
+        helper_text: "Please enter your password"
+        helper_text_mode: "on_error"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.35}
         size_hint_x: None
         width: self.parent.width / 1.5
     
     MDRectangleFlatButton:
+        id: login_button
         text: 'Login'
-        pos_hint: {'center_x': 0.5, 'center_y': 0.3}
+        pos_hint: {'center_x': 0.5, 'center_y': 0.2}
 """
 
 Window.size = (300, 500)
@@ -39,10 +45,41 @@ Window.size = (300, 500)
 
 class LocationApp(MDApp):
 
-    def build(self):
-        screen = Builder.load_string(helper)
+    email = StringProperty("")
+    password = StringProperty("")
+    screen = None
 
-        return screen
+    def build(self):
+        self.screen = Builder.load_string(helper)
+        self.screen.ids.user_email.bind(
+            on_text_validate=self.login
+        )
+        self.screen.ids.user_password.bind(
+            on_text_validate=self.login
+        )
+        self.screen.ids.login_button.bind(
+            on_release=self.login
+        )
+
+        return self.screen
+
+    def login(self, args):
+        try:
+            if self.screen.ids.user_email.text == "":
+                self.screen.ids.user_email.error = True
+                raise ValueError
+
+            if self.screen.ids.user_password.text == "":
+                self.screen.ids.user_password.error = True
+                raise ValueError
+
+            # TODO: check if username and password are registered
+
+            self.email = self.screen.ids.user_email.text
+            self.password = self.screen.ids.user_password.text
+
+        except ValueError:
+            pass
 
 
 LocationApp().run()
